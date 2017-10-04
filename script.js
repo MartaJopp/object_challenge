705467f8af303efd4d0f5f9728204287a2c3c537
Marta Jopp <martapeterson@gmail.com>
2017-10-03 21:25:49 -0500

    changes

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 var locations = []; // this array will hold your objects
 
 // object constructor
-
+var State = function(stateInput, revenueInput, locationCountInput){
+  this.state = stateInput;
+  this.revenue = revenueInput;
+  this.locationCount = locationCountInput;
+  this.averageRevenue = function (){
+    return this.revenue/this.locationCount;
+  };
+};
 // object instances
-
+var illinois = new State ('IL', 5000, 12);
+var minnesota = new State ('MN', 300, 3);
+var nevada = new State ('NV', 25000, 1);
 // push object instances to locations array
-
+locations.push(illinois, minnesota, nevada);
 // DO NOT MODIFY THIS CODE
 $(document).ready(function () {
   locations.forEach(function (element, index) {
